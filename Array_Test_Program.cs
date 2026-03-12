7416cc89fbd7b8e694c99a1d1cbef0504217eea7
AnkitaHeralagi <126943633+AnkitaHeralagi@users.noreply.github.com>
2026-03-12 16:40:40 +0530

    Add files via upload

---
+++ Array_Test_Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayTestProgram
+{
+    class ArrayTest
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Ankita", "Akul", "Priya", "Amit", "Neha" };
+
+            Console.WriteLine("Printing using FOR loop:");
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("Index: " + i + "  Name: " + names[i]);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Printing using FOREACH loop:");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine("Name: " + name);
+            }
+        }
+    }
+}
